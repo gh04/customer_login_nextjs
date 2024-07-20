1b8f3a4bf70ccc49beb825d2c06d0a4559019368
Gerardo Hernandez <gahernandez02@gmail.com>
2024-07-19 22:51:04 -0700

    Updated org name in landing page

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,11 @@ export default function Page() {
             className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
           />
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Valencia.</strong> Home{' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+              of the Greatest Family Alive
             </a>
-            , brought to you by Vercel.
+            , brought to you by Gerardo & Janee.
           </p>
           <Link
             href="/login"
